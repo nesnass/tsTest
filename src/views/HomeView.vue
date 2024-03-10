1f11ff22e324787e8b425ca7fa88422eb3a1f3b2
Richard Nesnass <richard.nesnass@uv.uio.no>
2024-03-10 22:54:58 +0100

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import { Draggable } from '@braks/revue-draggable'
+</script>
+
+<template>
+  <main>
+    <Draggable />
+    <TheWelcome />
+  </main>
+</template>
